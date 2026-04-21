56fdac48845ba1943d67410ce193ef5fb7d78fc8
squillwah <Swagapotato@gmail.com>
2026-04-21 09:59:19 -0400

    Tier 1: stupid sort

---
+++ tier1/bubbsort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+//#define LISTSIZE 100000000
+#define LISTSIZE 100000
+// Make sure to unlimit your stack: ulimit -s unlimited
+
+int main(void) {
+    srand(time(0));
+
+    int nums[LISTSIZE];
+    for (int i = 0; i < LISTSIZE; i++) 
+        nums[i] = rand() % LISTSIZE*10;
+
+    std::cout << "(";
+    for (int i = 0; i < LISTSIZE; i++) std::cout << nums[i] << " ";
+    std::cout << ")" << std::endl;
+
+    int i;
+    int tmp;
+    int sorted = 0;
+    while (!sorted) {
+        sorted = 1;
+        for (i = 0; i < LISTSIZE-1; i++) {
+            if (nums[i] > nums[i+1]) {
+                sorted = 0;
+                tmp = nums[i+1];
+                nums[i+1] = nums[i];
+                nums[i] = tmp;
+            }
+        }
+    }
+    
+    std::cout << "(";
+    for (int i = 0; i < LISTSIZE; i++) std::cout << nums[i] << " ";
+    std::cout << ")" << std::endl;
+
+    return 0;
+}
